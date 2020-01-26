b35ba0211b5c61f986d99d9de02029165609a915
himselfv@gmail.com <himselfv@gmail.com>
2020-01-26 20:18:58 +0300

    mirandadb: upgraded mirrestore a little

--- mirrestore.py
+++ mirrestore.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 import coreutils
 import mirandadb
+import mirdiff
 import utfutils
 import fnmatch
 
@@ -43,47 +44,51 @@ class MirandaDbxMmapChk(mirandadb.MirandaDbxMmap):
 		"""
 		return ret
 
-
-bad_event_count = 0
-bad_offsets = {}		# Bad event offset statistics
-
-def dump_events(db, contact):
-	print "Events for "+contact.display_name+": "
-	global bad_event_count
-	global bad_offsets
-	for event in db.get_events(contact):
-		data = event.data
-		if hasattr(data, 'problem'):
-			bad_event_count += 1
-		if args.bad_events and not hasattr(data, 'problem'):
-			continue
-		if args.bad_offsets:
-			data.offset = event.offset
-			bad_offset = event.offset // 0x10000
-			if bad_offset in bad_offsets:
-				bad_offsets[bad_offset] += 1
-			else:
-				bad_offsets[bad_offset] = 1
-		print mirandadb.format_event(db, event, data)
-
-# Main
-parser = argparse.ArgumentParser(description="Analyzes Miranda database for corruption.",
-	parents=[coreutils.argparser()])
-parser.add_argument("dbname", help='path to database file')
-parser.add_argument("--dump-events", help='prints all events for the given contact', metavar='contact_mask', type=str, action='append')
-parser.add_argument("--bad-events", help='dumps bad events only', action='store_true')
-parser.add_argument("--bad-offsets", help='gathers bad event offset statistics', action='store_true')
-args = parser.parse_args()
-coreutils.init(args)
-
-db = MirandaDbxMmapChk(args.dbname)
-
-if args.dump_events:
-	for contact_name in args.dump_events:
-		for contact in db.contacts_by_mask(contact_name):
-			dump_events(db, contact)
+def dump_events(db, args):
+	bad_event_count = 0
+	bad_offsets = {}
+	for contact in mirandadb.select_contacts_opt(db, args.contact):
+		print "Events for "+contact.display_name+": "
+		for event in db.get_events(contact):
+			data = event.data
+			if hasattr(data, 'problem'):
+				bad_event_count += 1
+			if args.bad and not hasattr(data, 'problem'):
+				continue
+			if args.bad_offsets:
+				data.offset = event.offset
+				bad_offset = event.offset // 0x10000
+				if bad_offset in bad_offsets:
+					bad_offsets[bad_offset] += 1
+				else:
+					bad_offsets[bad_offset] = 1
+			print mirandadb.format_event(db, event, data)
 	log.warning("Bad events: "+str(bad_event_count))
 	print "Bad events:"+str(bad_event_count)
 	if args.bad_offsets:
 		print "Bad offsets:"
 		print '\n'.join([ repr(key) + ': ' + repr(value) for (key, value) in bad_offsets.items()])
+
+
+# Main
+parser = argparse.ArgumentParser(description="Analyzes Miranda database for corruption.",
+	parents=[coreutils.argparser()])
+parser.add_argument("dbname", help='path to database file')
+subparsers = parser.add_subparsers(title='subcommands')
+
+sparser = subparsers.add_parser('dump-events', help='prints all events for the given contacts')
+sparser.add_argument('contact', type=str, nargs='*', help='print events for these contacts')
+sparser.add_argument("--bad", help='dumps only bad events', action='store_true')
+sparser.add_argument("--bad-offsets", help='gathers bad event offset statistics', action='store_true')
+sparser.set_defaults(func=dump_events)
+
+sparser = subparsers.add_parser('restore-events', help='restores events which differ from another DB')
+
+args = parser.parse_args()
+coreutils.init(args)
+
+db = MirandaDbxMmapChk(args.dbname)
+	
+if args.func <> None:
+	args.func(db, args)
+
